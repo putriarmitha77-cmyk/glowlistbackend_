4c8b056dfd0d01fbfbdac3f7cf5372c5d467f23e
putriarmitha77-cmyk <putriarmitha77@gmail.com>
2026-08-04 12:41:25 +0700

    tambahkan get kategori

--- server.js
+++ server.js
@@ -1,12 +1,50 @@
 const express = require('express');
 const app = express();
+const mysql2 = require('mysql2');
 const PORT =3001;
+
+const db = mysql2.createConnection({
+    host : 'Localhost',
+    user : 'root',
+    password : '',
+    database : 'glowlist_db'
+});
+
+db.connect(err => {
+    if(err) {
+        console.error('Koneksi ke database gagal:', err);
+        }else {
+        console.log('Berhasil konek ke database Glowlist');
+        }
+});
+
 
 app.use(express.json());
 
 app.get('/', (req, res)=> {
     res.send('Selamat Datang Di Glowlist API!!');
 });
+
+
+//=======GET PRODUK======//
+app.get('/produk', (req, res) => {
+    const sql = 'SELECT * FROM produk';
+    db.query(sql, (err, results) => {
+        if(err) return res.status(500).json({ error: err });
+        res.json(results);
+    });
+});
+
+//======GET KATEGORI=====//
+app.get('/kategori', (req, res) => {
+    const sql = 'SELECT * FROM kategori';
+    db.query(sql, (err, results) => {
+        if(err) return res.status(500).json({ error: err });
+        res.json(results);
+    });
+});
+
+//---------------------//
 
 app.listen(PORT, () => {
     console.log(`Server Glowlist berjalan di http://localhost:${PORT}`);
