06369e86efc11c947d0da59c907479859cba3546
putriarmitha77-cmyk <putriarmitha77@gmail.com>
2026-08-19 09:45:32 +0700

    upload file

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 const mysql2 = require('mysql2');
 const PORT =3001;
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const authJWT = require('./middleware');
 const saltRounds = 10;
 
 const db = mysql2.createConnection({
@@ -119,8 +121,42 @@ app.post('/pengguna', async (req, res) => {
     }
 });
 
+//======POST LOGIN======//
+app.post('/login', (req, res) => {
+    const { email, password } = req.body || {};
+
+    if (!email || !password) {
+        return res.status(400).json({
+            message: 'Email dan password wajib diisi'
+        });
+    }
+    const sql = 'SELECT * FROM pengguna WHERE email = ?';
+
+    db.query(sql, [email], (err, result) => {
+        if (err) return res.status(500).json({ error: sqlMessage });
+        if (result.length === 0) {
+            return res.status(404).json({ message: 'Akun tidak ditemukan' });
+        }
+
+        const user = result[0];
+        const passwordIsValid = bcrypt.compareSync(password, user.password);
+
+        if (!passwordIsValid) {
+            return res.status(401).json({ message: 'Password salah'});
+        }
+
+        const token = jwt.sign(
+            { id: user.id_pengguna }, 
+            'glowlistrahasia',
+            { expiresIn: 86400 } 
+        );
+
+        res.status(200).json({auth: true, token, id_pengguna: user.id_pengguna, nama: user.nama});
+    })
+})
+
 //=====DELETE PRODUK=====//
-app.delete('/produk/:id_produk', (req, res) => {
+app.delete('/produk/:id_produk', authJWT, (req, res) => {
     const { id_produk } = req.params;
     const sql = 'DELETE FROM produk WHERE id_produk=?';
     db.query(sql, [id_produk], (err, results) => {
@@ -220,6 +256,21 @@ app.delete('/kategori/:id_kategori', (req, res) => {
         res.json({ message: 'Kategori berhasil dihapus!!' });
     });
 }) ;
+
+//======GET PENGGUNA=====//
+app.get('/pengguna/me', authJWT, (req, res) => {
+    const id = req.user.id;
+    const sql = `SELECT id_pengguna, nama, email, no_hp FROM pengguna WHERE id_pengguna = ?`;
+    db.query(sql, [id], (err, result) => {
+        if (err) {
+            return res.status(500).json({ error: err.sqlMessage });
+        }
+        if (result.length === 0) {
+            return res.status(404).json({ message: 'Pengguna tidak ditemukan' });
+        }
+        res.json(result[0]);
+    });
+});
 
 
 //---------------------//
